0c979c4f902eab61721f96df327bc1541234e69d
Kaggl <rkaggl@oeaw.ads>
2019-03-05 12:57:47 +0100

    some improvements

--- solr.js
+++ solr.js
@@ -1,10 +1,18 @@
   $(".loader").hide();
   const t = $('#table').DataTable({
-    "columns": [
-      { "data": "meta_title" },
-      { "data": "highlight" },
-      { "data": "content_type" },
-    ]
+    "columns": [{
+        "data": "meta_title"
+      },
+      {
+        "data": "highlight"
+      },
+      {
+        "data": "content_type"
+      },
+    ],
+    // "processing": true,
+    // "serverSide": true,
+    // "ajax": "processor.php",
   });
 
   function getSolr(params) {
@@ -21,7 +29,19 @@
     $.getJSON(params.solrEndpoint, header, (obj) => {
       numFound = obj.response.numFound;
       console.log(obj);
+
       for (let i = 0; i < obj.response.docs.length; i += 1) {
+        let inCollection = false;
+
+        if (obj.response.docs[i].meta_identifier) {
+          for (let j = 0; j < obj.response.docs[i].meta_identifier.length; j += 1) {
+            if (obj.response.docs[i].meta_identifier[j].indexOf(params.searchInCollection) > -1) {
+              console.log("put in col", obj.response.docs[i].meta_identifier[j]);
+              inCollection = true;
+            }
+          }
+        }
+        if (!inCollection) continue;
         if (obj.highlighting[obj.response.docs[i].id]._text_) {
           obj.response.docs[i].highlight = obj.highlighting[obj.response.docs[i].id]._text_[0]
             .replace(/\n|^\W|^\D/g, '')
@@ -31,6 +51,7 @@
         if (!obj.response.docs[i].meta_title) obj.response.docs[i].meta_title = 'no title given';
         ret.push(obj.response.docs[i]);
       }
+
       $(".loader").hide();
       t.data().clear();
       t.rows.add(ret).draw();
