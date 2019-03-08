b9e3899ed118392d227a9d0fcedbad05b04746ca
Mateusz Żółtak <zozlak@zozlak.org>
2019-03-08 14:53:42 +0100

    SPARQL search added
    The getSolr() params structure can now contain `sparqlEndpoint` and `sparqlQuery` properties.
    If so, results will contain only entries matching both Solr and SPARQL query results.
    The SPARQL query must fetch the `resUrl` variable containing matching resource URLs.
    E.g. to limit search to resources in the `https://id.acdh.oeaw.ac.at/staribacher` collection:
    `sparqlQuery: 'select ?resUrl where {?resUrl (<https://vocabs.acdh.oeaw.ac.at/schema#isPartOf> / ^<https://vocabs.acdh.oeaw.ac.at/schema#hasIdentifier>)* / <https://vocabs.acdh.oeaw.ac.at/schema#hasIdentifier> <https://id.acdh.oeaw.ac.at/staribacher>}'`
    
    Previous `searchInCollection` property support has been removed as it basically didn't work.

--- solr.js
+++ solr.js
@@ -23,40 +23,62 @@
       rows: params.pageSize,
       // sort: 'meta_title asc',
     }
-    const ret = [];
-    let numFound;
+    let ret = [];
+    let sparqlRet = (params.sparqlQuery ? [] : null);
+    let reqToGo = 1 + (params.sparqlQuery ? 1 : 0);
+    
+    let combineResults = function() {
+      reqToGo--;
+      if (reqToGo <= 0) {
+        if (sparqlRet !== null) {
+        console.log(ret);
+          const ret2 = [];
+          for (let i = 0; i < ret.length; i++) {
+            if (sparqlRet.indexOf(ret[i].id) >= 0) {
+              ret2.push(ret[i]);
+            }
+          }
+          ret = ret2;
+        }
+      
+        $(".loader").hide();
+        t.data().clear();
+        t.rows.add(ret).draw();
+        const numFound = ret.length;
+        $("#output").html(`${ret.length} results usable, ${numFound} total\n`);
+      }
+    };
+    
     $(".loader").show();
+    
     $.getJSON(params.solrEndpoint, header, (obj) => {
-      numFound = obj.response.numFound;
       console.log(obj);
 
       for (let i = 0; i < obj.response.docs.length; i += 1) {
-        if (params.searchInCollection) {
-          let inCollection = false;
-          if (obj.response.docs[i].meta_identifier) {
-            for (let j = 0; j < obj.response.docs[i].meta_identifier.length; j += 1) {
-              if (obj.response.docs[i].meta_identifier[j].indexOf(params.searchInCollection) > -1) {
-                console.log("put in col", obj.response.docs[i].meta_identifier[j]);
-                inCollection = true;
-              }
-            }
-          }
-          if (!inCollection) continue;
-        }
         if (obj.highlighting[obj.response.docs[i].id]._text_) {
           obj.response.docs[i].highlight = obj.highlighting[obj.response.docs[i].id]._text_[0]
             .replace(/\n|^\W|^\D/g, '')
             .replace(/\s+/g, ' ')
             .trim();
-        } else obj.response.docs[i].highlight = 'no highlighting given';
-        if (!obj.response.docs[i].meta_title) obj.response.docs[i].meta_title = 'no title given';
+        } else {
+          obj.response.docs[i].highlight = 'no highlighting given';
+        }
+        if (!obj.response.docs[i].meta_title) {
+          obj.response.docs[i].meta_title = 'no title given';
+        }
         ret.push(obj.response.docs[i]);
       }
 
-      $(".loader").hide();
-      t.data().clear();
-      t.rows.add(ret).draw();
-
-      $("#output").html(`${ret.length} results usable, ${numFound} total\n`);
+      combineResults();
     });
+    
+    if (params.sparqlQuery) {
+      $.getJSON(params.sparqlEndpoint, {query: params.sparqlQuery}, (obj) => {
+        console.log(obj);
+        for (let i = 0; i < obj.results.bindings.length; i++) {
+          sparqlRet.push(obj.results.bindings[i].res.value);
+        }
+        combineResults();
+      });
+    }
   }
